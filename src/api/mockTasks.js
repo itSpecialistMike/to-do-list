1b7e70895ad5041e140e0e8277d2cd30a92189bf
Михаил Матафонов <mihailmatafonov@Mihails-MacBook-Pro.local>
2025-07-17 19:37:40 +0700

    настроил для работы с мок на локалке и с апи на проде

---
+++ src/api/mockTasks.js
@@ -0,0 +1,16 @@
+// src/api/mockTasks.js
+
+export const mockTasks = [
+  {
+    id: 1,
+    title: 'Покормить кота (мок)',
+    description: 'Это фейковая задача для локальной разработки.',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: 'Полить цветы (мок)',
+    description: 'Еще одна моковая задача.',
+    completed: true,
+  },
+]
